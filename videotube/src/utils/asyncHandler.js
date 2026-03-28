269c763af882730547dd1597b9abee82032b9c35
Harsh Nagar <harsh96037@gmail.com>
2026-03-28 12:10:58 +0530

    add user and video model

--- videotube/src/utils/asyncHandler.js
+++ videotube/src/utils/asyncHandler.js
@@ -1,10 +1,14 @@
 const asyncHandler = (requestHandler) => {
   (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => err);
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => (err));
   };
 };
 
 export { asyncHandler };
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => ()=> {}
+// const asyncHandler = (func) => async () => {}
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
