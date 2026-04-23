881938187cf14977f78cba30810b54ebc2435fac
Harsh Nagar <harsh96037@gmail.com>
2026-04-24 01:01:47 +0530

    all setup configured

--- videotube/src/index.js
+++ videotube/src/index.js
@@ -2,14 +2,13 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from './app.js'
 
-dotenv.config({
-  path: "./env",
-});
+dotenv.config();
 
 connectDB()
 .then(()=> {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000  , () => {
         console.log(` Server is running at port: ${process.env.PORT}`);
         
     })
--- videotube/src/models/user.model.js
+++ videotube/src/models/user.model.js
@@ -32,10 +32,10 @@ const userSchema = new Schema(
         coverImage: {
             type: String, // cloudinary url
         },
-        watchHistory: {
+        watchHistory: [{
             type: Schema.Types.ObjectId,
             ref: "Video"
-        },
+        }],
         password: {
             type: String,
             required: [true, 'Password is required']
