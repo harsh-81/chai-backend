0f74decf3d41c54e1d66a0191529090a2168e3f6
Harsh Nagar <harsh96037@gmail.com>
2026-05-03 00:16:04 +0530

    fix: update refreshAccessToken logic to check for missing token feat: add subscription model for user subscriptions

---
+++ videotube/src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schemma} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref: "User"
+    }, 
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom subscriber is subscribing
+        ref: "User"
+    }
+}, { timeStamps: true })
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
